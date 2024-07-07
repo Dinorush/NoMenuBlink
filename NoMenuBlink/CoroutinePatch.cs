fdc465248422773775e00a5ac6b853bb735e39d7
Dinorush <onparnorthstar@gmail.com>
2024-07-06 21:18:51 -0400

    Add project files.

---
+++ NoMenuBlink/CoroutinePatch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections;
+using UnityEngine;
+using CollectionExtensions = BepInEx.Unity.IL2CPP.Utils.Collections.CollectionExtensions;
+
+namespace ColorCrosshair
+{
+    [HarmonyPatch]
+    internal static class CoroutinePatch
+    {
+        [HarmonyPatch(typeof(CoroutineManager), nameof(CoroutineManager.BlinkIn), new Type[] {typeof(GameObject), typeof(float)})]
+        [HarmonyPrefix]
+        public static bool CancelBlink(GameObject go, ref Coroutine __result)
+        {
+            __result = CoroutineManager.StartCoroutine(CollectionExtensions.WrapToIl2Cpp(EmptyRoutine()));
+            if (go != null)
+                go.SetActive(true);
+            return false;
+        }
+
+        private static IEnumerator EmptyRoutine()
+        {
+            yield return null;
+        }
+    }
+}
